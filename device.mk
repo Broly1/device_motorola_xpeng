b093d067387c8adeb07a1b294a0a3749557cfd06
Cosmin Tanislav <demonsingur@gmail.com>
2022-09-21 10:06:15 +0530

    berlna: Add dummy sensors sub HAL
    Change-Id: Ie20ed775344990de3e16e2eefba1754d831027fb

--- device.mk
+++ device.mk
@@ -98,6 +98,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml
 
 # Sensors
+PRODUCT_PACKAGES += \
+    sensors.berlna
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
